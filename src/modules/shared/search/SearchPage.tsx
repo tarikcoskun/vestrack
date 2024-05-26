7bb77b4603f5d074039fc36c1655628d52755eae
Tarık Coşkun <tarikcskun@gmail.com>
2024-05-26 13:38:33 +0300

    search skeleton & fallback

--- src/modules/shared/search/SearchPage.tsx
+++ src/modules/shared/search/SearchPage.tsx
@@ -12,12 +12,14 @@ import { SearchTitleCard } from "./components/Card/Title";
 import style from "./SearchPage.module.scss";
 import classNames from "classnames/bind";
 import { SearchPersonCard } from "./components/Card/Person";
+import { Skeleton } from "@/components/Skeleton";
 
 const cx = classNames.bind(style);
 
 export function SearchPage() {
   const searchParams = useSearchParams();
   const query = searchParams.get("q")!;
+  const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState<SearchResponse | null>(null);
 
   useEffect(() => {
@@ -27,6 +29,8 @@ export function SearchPage() {
       })
       .catch((err) => {
         notifyError(err);
+      }).finally(() => {
+        setLoading(false);
       });
 
     async function fetchData() {
@@ -39,21 +43,39 @@ export function SearchPage() {
       <section className={cx("resultContainer")}>
         <h1>Titles</h1>
         <div className={cx("resultList")}>
-          {data?.titles.map((title) => (
-            <SearchTitleCard key={title.id} movie={title} type={title.media_type as "movie" | "tv"} />
-          ))}
+          {isLoading
+            ? Array(6).fill(0).map((_, idx) => <SearchCardSkeleton key={idx} />)
+            : data?.titles.length ? data?.titles.map((title) => (
+              <SearchTitleCard key={title.id} movie={title} type={title.media_type as "movie" | "tv"} />
+            )) : (
+              <h3>No results</h3>
+            )}
         </div>
       </section>
       <section className={cx("resultContainer")}>
         <h1>People</h1>
         <div className={cx("resultList")}>
-          {data?.people.map((person) => (
-            <SearchPersonCard key={person.id} person={person} />
-          ))}
+          {isLoading
+            ? Array(6).fill(0).map((_, idx) => <SearchCardSkeleton key={idx} />)
+            : data?.people.length ? data?.people.map((person) => (
+              <SearchPersonCard key={person.id} person={person} />
+            )) : (
+              <h3>No results</h3>
+            )}
         </div>
       </section>
     </main>
   );
 }
 
-
+function SearchCardSkeleton() {
+  return (
+    <div style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
+      <Skeleton width={80} height={120} />
+      <div>
+        <Skeleton width={120} height={18.11} type="text" />
+        <Skeleton width={180} height={16.09} type="text" style={{ marginTop: "0.5rem" }} />
+      </div>
+    </div>
+  );
+}
