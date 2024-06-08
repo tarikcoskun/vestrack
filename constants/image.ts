d4a9065ed81b262aa484d59ec127400fcd178d80
Tarık Coşkun <tarikcskun@gmail.com>
2024-06-08 13:19:38 +0300

    use optimized backdrop image

--- constants/image.ts
+++ constants/image.ts
@@ -1,6 +1,6 @@
 export const TMDB_IMAGE_BASE_POSTER = "https://image.tmdb.org/t/p/w300_and_h450_bestv2/";
 export const TMDB_IMAGE_BASE_POSTER_BLUR = "https://image.tmdb.org/t/p/w300_and_h450_multi_faces_filter%28blur%29/";
 export const TMDB_IMAGE_BASE_PERSON_PHOTO = "https://image.tmdb.org/t/p/w138_and_h175_face/";
-export const TMDB_IMAGE_BASE_BACKDROP = "https://image.tmdb.org/t/p/original";
+export const TMDB_IMAGE_BASE_BACKDROP = "https://image.tmdb.org/t/p/w1920_and_h1080_multi_faces";
 export const YOUTUBE_THUMBNAIL_BASE = "https://i.ytimg.com/vi/";
 export const YOUTUBE_THUMBNAIL_QUALITY = "/hqdefault.jpg";
